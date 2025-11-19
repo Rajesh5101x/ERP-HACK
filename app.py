df836aab1180efcdb99cdb0b99d963008091db30
Rajesh5101x <96674992+Rajesh5101x@users.noreply.github.com>
2025-11-19 22:28:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def format_marks(all_marks):
 
 
 def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": chat_id, "text": text, "parse_mode": "Markdown"}
     requests.post(url, json=payload)
 
